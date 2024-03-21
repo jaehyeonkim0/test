e18f99b9654854cfcd9c720cedf65d37530e7087
jaehyeonkim0 <akfakfhkd@naver.com>
2024-03-21 22:26:39 +0900

    mypage part start

--- src/main/java/com/movie/reservation/controller/mypage/MyPageController.java
+++ src/main/java/com/movie/reservation/controller/mypage/MyPageController.java
@@ -1,10 +1,14 @@
 package com.movie.reservation.controller.mypage;
 
+import com.movie.reservation.config.session.SessionConstant;
+import com.movie.reservation.dto.mypage.MemberDto;
 import com.movie.reservation.service.mypage.MyPageService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +20,14 @@ public class MyPageController {
 
     private final MyPageService myPageService;
 
-
     @GetMapping("/mypage")
-    public void mypageGET() {
-        System.out.println("Git Fetch 테스트중입니다2");
+    public String mypageGET(HttpSession session, Model model) {
+        //TODO 로그인한 사용자 커스텀 어노테이션 생성해서 수정
+        String username = (String) session.getAttribute(SessionConstant.LOGIN_MEMBER);
+
+        MemberDto memberDto = myPageService.getMemberDto(username);
+
+
+        return "member/mypage/mypage";
     }
 }
--- src/main/java/com/movie/reservation/service/mypage/MyPageServiceImpl.java
+++ src/main/java/com/movie/reservation/service/mypage/MyPageServiceImpl.java
@@ -1,13 +1,18 @@
 package com.movie.reservation.service.mypage;
 
 import com.movie.reservation.dto.mypage.MemberDto;
+import com.movie.reservation.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MyPageServiceImpl implements MyPageService {
 
+    private final MemberRepository memberRepository;
+
     @Override
-    public MemberDto getMemberDto() {
+    public MemberDto getMemberDto(String username) {
         return null;
     }
 }
