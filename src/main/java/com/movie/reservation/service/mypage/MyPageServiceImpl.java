51ddac922fac4d7889ade61d468e63b55a23282b
jaehyeonkim0 <akfakfhkd@naver.com>
2024-03-21 16:51:41 +0900

    first branch commit

---
+++ src/main/java/com/movie/reservation/service/mypage/MyPageServiceImpl.java
@@ -0,0 +1,13 @@
+package com.movie.reservation.service.mypage;
+
+import com.movie.reservation.dto.mypage.MemberDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyPageServiceImpl implements MyPageService{
+
+    @Override
+    public MemberDto getMemberDto() {
+        return null;
+    }
+}
